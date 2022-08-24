0e58cf787f24e9e1bccdf31051475d08f3621352
EdwinDC-ux <edwin9602@gmail.com>
2022-08-24 18:11:06 -0500

    Números

---
+++ numeros.py
@@ -0,0 +1,27 @@
+# Enteros
+num_entero = 8
+
+# Negativos
+num_negativo = -78
+
+# Reales (equivalen al tipo double de C)
+num_real = 4.5
+
+# Complejos
+num_complejo = 3.2 + 7j
+# O
+num_complejo =  5J + 3
+
+# Notación científica 0.5*10^-7
+num_real = 0.5e-7
+
+# Binarios (7)
+num_binario = 0b111
+
+# Octales (8)
+num_octal = 0o10
+
+# Hexadecimales (255)
+num_hex = 0xff
+
+print(16 * 16 - 1)
