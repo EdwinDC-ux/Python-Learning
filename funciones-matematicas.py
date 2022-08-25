55d5218cb05fe610eaebdab7d2118dd4834ca73b
EdwinDC-ux <edwin9602@gmail.com>
2022-08-24 19:49:04 -0500

    Funciones matemáticas

---
+++ funciones-matematicas.py
@@ -0,0 +1,16 @@
+# Para algunas funciones se requiere importar un módulo llamado math
+import math
+
+# Valor absoluto
+x = abs(-47.67)
+print(x)
+
+# Raíz cuadrada
+x = math.sqrt(169)
+print(x)
+
+# Cambio de base
+# Acepta cualquier representación admitida por python
+print(hex(16))
+print(oct(8))
+print(bin(0xfe))
